d5ab972f99431bccfd72be3a0d60e4eb17cb07b4
Joe Scott <jbs198585@gmail.com>
2022-10-07 07:47:08 -0400

    String Reversal

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,12 +2,32 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// SOLUTION #3
 function reverse(str) {
-  
+  return str.split("").reduce((rev, char) => char + rev, "");
 }
 
 module.exports = reverse;
+
+// SOLUTION #1
+// function reverse(str) {
+//   return str
+//     .split("")
+//     .reverse()
+//     .join("");
+// }
+
+// SOLUTION #2
+// function reverse(str) {
+//   let reversed = "";
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }

3f119626a91f39d776ad04525193d8926fd20ae8
Joe Scott <jbs198585@gmail.com>
2022-10-18 06:55:08 -0400

    fizzbuzz solution

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -17,6 +17,9 @@ function fizzBuzz(n) {
     // Is the number a multiple of 3 and 5?
     if (i % 3 === 0 && i % 5 === 0) {
       console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      // Is the number a multiple of 3?
+      console.log("fizz");
     }
   }
 }
